f6b1a2bf4b5718e7cfcf2a3dafd0ea267a3b9696
NaveenKumar-008 <128135244+NaveenKumar-008@users.noreply.github.com>
2023-03-24 21:33:21 +0530

    Update Exp2.py

--- Exp2.py
+++ Exp2.py
@@ -1,64 +1,38 @@
-import pandas as ps
-import numpy as np
-import seaborn as sns
+import pandas as pd
+from google.colab import files
+uploaded = files.upload()
+df = pd.read_csv("bhp.csv")
+Q1 = df['price_per_sqft'].quantile(0.25)
+Q3 = df['price_per_sqft'].quantile(0.75)
+IQR = Q3 - Q1
+lower_limit = Q1 - 1.5*IQR
+upper_limit = Q3 + 1.5*IQR
+df_iqr = df[(df['price_per_sqft'] >= lower_limit) & (df['price_per_sqft'] <=
+upper_limit)]
+print("New data:",df_iqr)
+from scipy.stats import zscore
+df_z = df_iqr[(zscore(df_iqr['price_per_sqft']) < 3)]
+print("Result",df_z) 
 
-df=ps.read_csv("bhp.csv")
-df
 
-df.head()
-df.describe()
-df.info()
-df.isnull().sum()
-df.shape
-
-sns.boxplot(x="price_per_sqft",data=df)
-
-q1=df['price_per_sqft'].quantile(0.35)
-q3=df['price_per_sqft'].quantile(0.65)
-print("First Quantile =",q1,"Second quantile =",q3)
-
-IQR=q3-q1 #INTERQUARTILE RANGE
-u1=q3+1.5*IQR
-l1=q1-1.5*IQR
-
-df1=df[((df['price_per_sqft']<=l1)&(df['price_per_sqft']>u1))]
-df1
-
-df1.shape
-
-sns.boxplot(x='price_per_sqft',data=df1)
-
-from scipy import stats
-z=np.abs(stats.zscore(df['price_per_sqft']))
-df2=df[(z<3)]
-df2
-
-print(df2.shape)
-
-sns.boxplot(x='price_per_sqft',data=df2)
-
-df3=ps.read_csv('height_weight.csv')
-df3
-
-df3.head()
-df3.info()
-df3.describe()
-df3.isnull().sum()
-df3.shape
-
-sns.boxplot(x='weight',data=df3)
-
-q1=df3['weight'].quantile(0.25)
-q3=df3['weight'].quantile(0.75)
-print('First Quantile =',q1,'Second Quantile =',q3)
-
-IQR=q3-q1
-u1=q3+1.5*IQR
-l1=q1-1.5*IQR
-
-df4 =df3[((df3['height']>=l1)&(df3['height']<=u1))]
-df4
-
-df4.shape
-
-sns.boxplot(x='height',data=df4)
+import pandas as pd
+from google.colab import files
+uploaded = files.upload()
+df = pd.read_csv("height_weight.csv")
+print(df.head())
+Q1 = df['weight'].quantile(0.25)
+Q3 = df['weight'].quantile(0.75)
+IQR = Q3 - Q1
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+outliers = df[(df['weight'] < lower_bound) | (df['weight'] >
+upper_bound)]
+print("Weight Outliers",outliers)
+Q1 = df['height'].quantile(0.25)
+Q3 = df['height'].quantile(0.75)
+IQR = Q3 - Q1
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+outliers = df[(df['height'] < lower_bound) | (df['height'] >
+upper_bound)]
+print("Height outliers",outliers)
